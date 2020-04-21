6c36e90e845891a3f79ae21f550fd3eba1602b52
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-04-21 10:56:52 +0100

    feat: Apply style guide styles to components

--- src/PageView/NearMe/SwitchButton/SwitchButton.js
+++ src/PageView/NearMe/SwitchButton/SwitchButton.js
@@ -3,7 +3,7 @@ import "./SwitchButton.css";
 
 export default function SwitchButton({ text, onClick }) {
   return (
-      <button className="SwitchButton button primary rounded-capsule" onClick={onClick}>
+      <button className="SwitchButton button primary" onClick={onClick}>
         Find your nearest {text}
       </button>
   );
