a494dd64753e16e3804681326a83f33035ab1b1e
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-04-21 15:20:54 +0100

    feat: Create IconAndText component

--- src/LandingPage/OurGoal/CentreImage/CentreImage.js
+++ src/LandingPage/OurGoal/CentreImage/CentreImage.js
@@ -1,40 +1,29 @@
-import React from "react";
-import "./CentreImage.css";
-import jogger from "../../../common/images/female-jogger.jpg";
-import { FaDumbbell, FaChartLine, FaHeartbeat } from "react-icons/fa";
+import React from 'react';
+import './CentreImage.css';
+import IconAndText from './IconAndText/IconAndText';
+import jogger from '../../../common/images/female-jogger.jpg';
 
 export default function CentreImage() {
     return (
         <div className="CentreImage">
-            <div className="container">
-                <div className="overlay">
-                    <div className="iconAndText">
-                        <div className="icon animated fadeInLeft delay-1s">
-                            <FaDumbbell />
-                        </div>
-                        <p className="slidingText animated fadeInRight delay-1s">
-                            ...Put you in touch with gyms and trainers nearby
-                        </p>
-                    </div>
-                    <div className="iconAndText">
-                        <div className="icon animated fadeInLeft delay-2s">
-                            <FaHeartbeat />
-                        </div>
-                        <p className="slidingText animated fadeInRight delay-2s">
-                            ...Deliver a workout routine tailored to your goals
-                        </p>
-                    </div>
-                    <div className="iconAndText">
-                        <div className="icon animated fadeInLeft delay-3s">
-                            <FaChartLine />
-                        </div>
-                        <p className="slidingText animated fadeInRight delay-3s">
-                            ...Keep track of your progress as you get healthier
-                        </p>
-                    </div>
-                </div>
-                <img className="image" src={jogger} alt="Jogger" />
+            <div className="overlay">
+                <IconAndText
+                    icon={"FaDumbbell"}
+                    text="...Put you in touch with gyms and trainers nearby"
+                    delay="1s"
+                />
+                <IconAndText
+                    icon={"FaChartLine"}
+                    text="..Deliver a workout routine tailored to your goals"
+                    delay="2s"
+                />
+                <IconAndText
+                    icon={"FaHeartbeat"}
+                    text="...Keep track of your progress as you get healthier"
+                    delay="3s"
+                />
             </div>
+            <img className="image" src={jogger} alt="Jogger" />
         </div>
     );
 }
---
+++ src/LandingPage/OurGoal/CentreImage/IconAndText/IconAndText.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./IconAndText.css";
+import { FaDumbbell, FaChartLine, FaHeartbeat } from "react-icons/fa";
+
+export default function IconAndText({ text, delay, icon }) {
+    return (
+        <div className="IconAndText">
+            <div className="iconAndText">
+                <div className={`icon animated fadeInLeft delay-${delay}`}>
+                    {icon === "FaDumbbell" ? (
+                        <FaDumbbell />
+                    ) : icon === "FaChartLine" ? (
+                        <FaChartLine />
+                    ) : (
+                        <FaHeartbeat />
+                    )}
+                </div>
+                <p
+                    className={`slidingText animated fadeInRight delay-${delay}`}
+                >
+                    {text}
+                </p>
+            </div>
+        </div>
+    );
+}
