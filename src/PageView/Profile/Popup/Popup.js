ce06a5fe349c5df8dc81e3a02e4a00624729d5c1
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-04-30 22:37:30 +0100

    feat: Create Popup stats

--- src/PageView/Profile/Popup/Popup.js
+++ src/PageView/Profile/Popup/Popup.js
@@ -4,7 +4,12 @@ import './Popup.css';
 export default function Popup() {
     return (
         <div className="Popup">
-            Popup title
+            <h2 className="title">Update Your Stats</h2>
+            <input type="number" placeholder="Current weight"/>
+            <input type="number" placeholder="Goal weight"/>
+            <input type="number" placeholder="Age"/>
+            <input type="number" placeholder="Height"/>
+            <input type="text" placeholder="Sex"/>
         </div>
     );
 }
