996ae22116f6d8b44bbbceabda0ee4cd5351ec2e
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-01 11:35:21 +0100

    feat: Add setGoalWeight function to Popup

--- src/PageView/Profile/Popup/Popup.js
+++ src/PageView/Profile/Popup/Popup.js
@@ -3,6 +3,11 @@ import "./Popup.css";
 import Input from "./Input/Input";
 
 export default function Popup() {
+
+    const setGoalWeight = (weight) => {
+        localStorage.goalWeight = weight;
+    }
+
     return (
         <div className="Popup">
             <h2 className="title">Update Your Stats</h2>
@@ -10,11 +15,7 @@ export default function Popup() {
                 <Input title="Age" type="number" placeholder="--" />
                 <Input title="Height" type="number" placeholder="--" />
                 <Input title="Current Weight" type="number" placeholder="--" />
-                <Input title="Goal Weight" type="number" placeholder="--" />
-            <select>
-                <option>Male</option>
-                <option>Female</option>
-            </select>
+                <Input title="Goal Weight" type="number" placeholder="--" onChange={e => setGoalWeight(e.target.value)} />
             </div>
             <button className="button primary">Submit</button>
         </div>
