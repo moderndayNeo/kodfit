d93513f6a4724d04743d35d1f34ba85a1309cdef
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-01 14:18:46 +0100

    feat: Replace placeholders with localStorage.data

--- src/PageView/Profile/Popup/Popup.js
+++ src/PageView/Profile/Popup/Popup.js
@@ -9,10 +9,10 @@ export default function Popup({ onClick, faded }) {
         <div className={`Popup ${faded && "opaque"}`}>
             <h2 className="title">Update Your Stats</h2>
             <div className="container">
-                <Input title="Age" type="number" placeholder="--" onChange={e => setAge(e.target.value)} />
-                <Input title="Height (cm)" type="number" placeholder="--" onChange={e => setHeight(e.target.value)} />
-                <Input title="Current Weight (kg)" type="number" placeholder="--" onChange={e => setCurrentWeight(e.target.value)} />
-                <Input title="Goal Weight (kg)" type="number" placeholder="--" onChange={e => setGoalWeight(e.target.value)} />
+                <Input title="Age" type="number" placeholder={ localStorage.age ? localStorage.age : "--"} onChange={e => setAge(e.target.value)} />
+                <Input title="Height (cm)" type="number" placeholder={ localStorage.height ? localStorage.height : "--"} onChange={e => setHeight(e.target.value)} />
+                <Input title="Current Weight (kg)" type="number" placeholder={ localStorage.currentWeight ? localStorage.currentWeight : "--"} onChange={e => setCurrentWeight(e.target.value)} />
+                <Input title="Goal Weight (kg)" type="number" placeholder={ localStorage.goalWeight ? localStorage.goalWeight : "--"} onChange={e => setGoalWeight(e.target.value)} />
             </div>
             <button className="button primary" onClick={onClick}>Update</button>
         </div>
