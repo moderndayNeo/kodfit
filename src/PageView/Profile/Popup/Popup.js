0c33c7518193fdbac47054f2303aef668c153fa4
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-01 16:12:42 +0100

    feat: Fade App on Popup

--- src/PageView/Profile/Popup/Popup.js
+++ src/PageView/Profile/Popup/Popup.js
@@ -1,20 +1,58 @@
 import React from "react";
 import "./Popup.css";
 import Input from "./Input/Input";
-import { setAge, setCurrentWeight, setHeight, setGoalWeight } from './updateSettings';
+import {
+    setAge,
+    setCurrentWeight,
+    setHeight,
+    setGoalWeight,
+} from "./updateSettings";
 
-export default function Popup({ onClick, faded }) {
-
+export default function Popup({ onClick }) {
     return (
-        <div className={`Popup ${faded && "opaque"}`}>
-            <h2 className="title">Update Your Stats</h2>
-            <div className="container">
-                <Input title="Age" type="number" placeholder={ localStorage.age ? localStorage.age : "--"} onChange={e => setAge(e.target.value)} />
-                <Input title="Height (cm)" type="number" placeholder={ localStorage.height ? localStorage.height : "--"} onChange={e => setHeight(e.target.value)} />
-                <Input title="Current Weight (kg)" type="number" placeholder={ localStorage.currentWeight ? localStorage.currentWeight : "--"} onChange={e => setCurrentWeight(e.target.value)} />
-                <Input title="Goal Weight (kg)" type="number" placeholder={ localStorage.goalWeight ? localStorage.goalWeight : "--"} onChange={e => setGoalWeight(e.target.value)} />
+        <div className="Popup">
+            <div className="popup-box">
+                <h2 className="title">Update Your Stats</h2>
+                <div className="container">
+                    <Input
+                        title="Age"
+                        type="number"
+                        placeholder={localStorage.age ? localStorage.age : "--"}
+                        onChange={(e) => setAge(e.target.value)}
+                    />
+                    <Input
+                        title="Height (cm)"
+                        type="number"
+                        placeholder={
+                            localStorage.height ? localStorage.height : "--"
+                        }
+                        onChange={(e) => setHeight(e.target.value)}
+                    />
+                    <Input
+                        title="Current Weight (kg)"
+                        type="number"
+                        placeholder={
+                            localStorage.currentWeight
+                                ? localStorage.currentWeight
+                                : "--"
+                        }
+                        onChange={(e) => setCurrentWeight(e.target.value)}
+                    />
+                    <Input
+                        title="Goal Weight (kg)"
+                        type="number"
+                        placeholder={
+                            localStorage.goalWeight
+                                ? localStorage.goalWeight
+                                : "--"
+                        }
+                        onChange={(e) => setGoalWeight(e.target.value)}
+                    />
+                </div>
+                <button className="button primary" onClick={onClick}>
+                    Update
+                </button>
             </div>
-            <button className="button primary" onClick={onClick}>Update</button>
         </div>
     );
 }
--- src/PageView/Profile/Profile.js
+++ src/PageView/Profile/Profile.js
@@ -6,23 +6,23 @@ import UserProfile from "./UserProfile/UserProfile";
 import Popup from "./Popup/Popup";
 
 export default function Profile() {
-    const [popup, setPopup] = useState(false);
-    
+    const [popup, setPopup] = useState(true);
+
     console.log(localStorage);
 
     return (
-        <div className={`Profile ${popup && "faded"}`}>
-            {popup && <Popup onClick={() => setPopup(!popup)} faded={!popup} />}
+        <div className="Profile">
             <button
                 className="button primary rounded-capsule update-stats"
                 onClick={() => setPopup(!popup)}
             >
                 Update your stats
             </button>
+            {popup && <Popup onClick={() => setPopup(!popup)} />}
             <UserProfile />
             <section className="stats">
                 <h2>Your Stats</h2>
-    <p>Goal weight: {localStorage.goalWeight}kg</p>
+                <p>Goal weight: {localStorage.goalWeight}kg</p>
                 <p>Current weight: {localStorage.currentWeight}kg</p>
                 <p>BMI</p>
                 <p>Target BMI</p>
