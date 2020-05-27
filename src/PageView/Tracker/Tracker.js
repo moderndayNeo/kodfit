5828e8d286a4f6b78cdadcdfa8b5f21121cc2825
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-27 14:45:30 +0100

    refactor: Re-styling for Tracker component

--- src/PageView/Tracker/Tracker.js
+++ src/PageView/Tracker/Tracker.js
@@ -11,22 +11,11 @@ console.log(localStorage)
   return (
     <div className='Tracker'>
       <h2 className='separator-center trackerTitle'>Tracker</h2>
-      <main className='container'>
-        <div className='center grid-full'>Your weight goals: {localStorage.goalWeight} kg</div>
-        <div className='center grid-full'>Your current weight: {localStorage.currentWeight} kg </div>
-        <div className='line-graph grid-full'>
+        <div>Goal Weight: {localStorage.goalWeight} kg</div>
+        <div>Current Weight: {localStorage.currentWeight} kg </div>
           <Line data={lineChartData} options={lineStyles} />
-        </div>
-        <div className='grid-full center'>April</div>
-        <div className='center grid-full'>Total time spent working out  5 hour 27mins</div>
-        <div className='grid-full center'>7 days </div>
-        <div className='grid-full center'>Total consecutive days 2 days</div>
-        <div className='grid-full  center small-font'> 7 days </div>
-        <div className=' grid-full'>
+
           <Bar data={barChartData} options={barStyles} />
-        </div>
-        <div className='grid-full center margin-bottom'>April</div>
-      </main>
     </div>
   )
 }
