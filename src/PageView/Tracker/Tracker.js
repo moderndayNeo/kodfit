7587689f4ca15e97fe2d4f066851a4eeec139342
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-27 17:34:13 +0100

    feat: Create, import months array

--- src/PageView/Tracker/Tracker.js
+++ src/PageView/Tracker/Tracker.js
@@ -4,15 +4,18 @@ import { Bar, Line } from 'react-chartjs-2'
 import { lineChartData, lineStyles, barStyles, barChartData } from './Data'
 import 'chartjs-plugin-datalabels'
 import WeightDetails from './WeightDetails/WeightDetails'
+import { months } from './months'
 
 export default function Tracker() {
-    console.log(localStorage)
+    const today = new Date()
+    const currentMonth = months[today.getMonth()]
 
     return (
         <div className="Tracker">
-            <h2 className="separator-center trackerTitle">Tracker</h2>
+            <h2 className="separator-center">Tracker</h2>
             <WeightDetails />
             <Line data={lineChartData} options={lineStyles} />
+            <p className="line-chart-title">{currentMonth}</p>
             <Bar data={barChartData} options={barStyles} />
         </div>
     )
