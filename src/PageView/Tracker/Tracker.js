835f899458393f8a74d8504e240df3d849c9b452
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-27 17:57:28 +0100

    feat: Create bar-chart-title in Tracker

--- src/PageView/Tracker/Tracker.js
+++ src/PageView/Tracker/Tracker.js
@@ -5,6 +5,7 @@ import { lineChartData, lineStyles, barStyles, barChartData } from './Data'
 import 'chartjs-plugin-datalabels'
 import WeightDetails from './WeightDetails/WeightDetails'
 import { months } from './months'
+import UserWorkoutStats from './UserWorkoutStats/UserWorkoutStats'
 
 export default function Tracker() {
     const today = new Date()
@@ -16,8 +17,12 @@ export default function Tracker() {
             <h2 className="separator-center">Tracker</h2>
             <WeightDetails />
             <Line data={lineChartData} options={lineStyles} />
-    <p className="line-chart-title">{currentMonth} {year}</p>
+            <p className="line-chart-title">
+                {currentMonth} {year}
+            </p>
+            <UserWorkoutStats />
             <Bar data={barChartData} options={barStyles} />
+            <p className="bar-chart-title">This Week</p>
         </div>
     )
 }
---
+++ src/PageView/Tracker/UserWorkoutStats/UserWorkoutStats.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './UserWorkoutStats.css'
+
+export default function UserWorkoutStats() {
+    return (
+        <div className="UserWorkoutStats">
+            <p>Total Time Working Out: 61 hours 42 minutes</p>
+            <p>Highest Consecutive Days: 12</p>
+        </div>
+    )
+}
