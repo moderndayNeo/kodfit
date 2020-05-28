d27bc5554bf0d0ee31dff49950a22945517f97f2
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-28 14:15:32 +0100

    feat: Create Switch Router for PageView

--- src/PageView/PageView.js
+++ src/PageView/PageView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CalendarApp from './CalendarApp/CalendarApp';
 import NearMe from './NearMe/NearMe';
 import WorkoutList from './WorkoutsMain/WorkoutList/WorkoutList';
@@ -19,24 +19,27 @@ import './PageView.css'
 export default function PageView() {
   return (
     <Router>
-      <div className="PageView">
-        <Route path="/" exact component={LandingPage} />
-        <section className="container">
-          <Route path="/login" exact component={Login} />
-          <Route path="/dashboard/calendar" exact component={CalendarApp} />
-          <Route path="/dashboard/near-me" exact component={NearMe} />
-          <Route path="/dashboard/workouts/:view/" exact component={WorkoutList} />
-          <Route path="/dashboard/workouts/home/:listId" exact component={ExerciseListDetails} />
-          <Route path="/dashboard/workouts/exercise-list/:listId" exact component={ExerciseListDetails} />
-          <Route path="/dashboard/workout-current" exact component={WorkoutCurrent} />
-          <Route path="/dashboard/tracker" exact component={Tracker} />
-          <Route path="/dashboard/timer" exact component={Timer} />
-          <Route path="/dashboard/stopwatch" exact component={StopWatch} />
-          <Route path="/dashboard/profile" exact component={Profile} />
-          <Route path="/dashboard/elements" exact component={Elements} />
-        </section>
-      </div>
-        <Route path="/dashboard/:slug" component={NotFound} />
+      <Switch />
+        <div className="PageView">
+          <Route path="/" exact component={LandingPage} />
+          <section className="container">
+            <Route path="/login" exact component={Login} />
+            <Route path="/dashboard/calendar" exact component={CalendarApp} />
+            <Route path="/dashboard/near-me" exact component={NearMe} />
+            <Route path="/dashboard/workouts/:view/" exact component={WorkoutList} />
+            <Route path="/dashboard/workouts/home/:listId" exact component={ExerciseListDetails} />
+            <Route path="/dashboard/workouts/exercise-list/:listId" exact component={ExerciseListDetails} />
+            <Route path="/dashboard/workout-current" exact component={WorkoutCurrent} />
+            <Route path="/dashboard/tracker" exact component={Tracker} />
+            <Route path="/dashboard/timer" exact component={Timer} />
+            <Route path="/dashboard/stopwatch" exact component={StopWatch} />
+            <Route path="/dashboard/profile" exact component={Profile} />
+            <Route path="/dashboard/elements" exact component={Elements} />
+          </section>
+        </div>
+        <Route path="/dashboard/:slug" component={Navbar} />
+        {/* <Route path="/:random" exact component={NotFound} /> */}
+      <Switch />
     </Router>
   );
 }
