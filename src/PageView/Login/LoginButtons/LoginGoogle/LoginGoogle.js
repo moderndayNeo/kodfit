e34f86dc6eaa9f4c0e210c59e43b7fe88e097c72
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-05-29 15:40:58 +0100

    refactor: Hide Google Login clientId

--- src/PageView/Login/LoginButtons/LoginGoogle/LoginGoogle.js
+++ src/PageView/Login/LoginButtons/LoginGoogle/LoginGoogle.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import './LoginGoogle.css';
+const API_clientID = `${process.env.REACT_APP_GOOGLE_LOGIN_CLIENT_ID}`
 
 export default function LoginGoogle({ onClick, onSuccess, onFailure }) {
+
+    console.log(API_clientID)
     return (
         <GoogleLogin
-            clientId="1097196745693-qh7n4kj5j6hc5nsrrhgdak0dm5kav4pb.apps.googleusercontent.com"
+            clientId={API_clientID}
             onSuccess={onSuccess}
             onFailure={onFailure}
             cookiePolicy={'single_host_origin'}
             render={(renderProps) => (
                 <button
-                    onClick={renderProps.onClick}
-                    disabled={renderProps.disabled}
+                    onClick={onClick}
                     className="button btn-transparent rounded-capsule LoginGoogle"
                 >
                     Sign in with Google
