417ae2b957dc4b4b6a3a4dc575e73fa02dcb76d5
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-06-12 17:56:29 +0100

    test: Saving to compare branches

--- src/PageView/Profile/PopupButton/PopupButton.js
+++ src/PageView/Profile/PopupButton/PopupButton.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './PopupButton.css';
 
 export default function PopupButton({ onClick, infoProvided }) {
-    
+    const [animated, setAnimated] = useState(false)
 
-    setInterval(() => {
-        
-    }, 5000);
+    useEffect(() => {
+        setInterval(() => {
+            if (infoProvided === false) {
+                setAnimated(true)
+            }
+        }, 3000);
+        setTimeout(() => {
+            setAnimated(false)
+        }, 1000);
+    }, [infoProvided])
 
     return (
         <button
-            className={`PopupButton button primary rounded-capsule update-stats ${!infoProvided && "bouncing"}`}
+            className={`PopupButton button primary rounded-capsule update-stats ${animated && "bouncing"}`}
             onClick={onClick}
         >
             Update your stats
