2981fc01c741186aab9de79a5a47651ed4f8ebc4
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-06-12 15:40:33 +0100

    refactor: Change default profile image

--- src/PageView/Profile/UserProfile/UserProfile.js
+++ src/PageView/Profile/UserProfile/UserProfile.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './UserProfile.css';
 import PersonalInfo from './PersonalInfo/PersonalInfo';
-// import defaultImage from '../../../common/images/default-user-image.png'
 import profileImage from '../../../common/images/default-profile-image.png'
+
 
 export default function UserProfile() {
     const userName = localStorage.getItem('userName');
