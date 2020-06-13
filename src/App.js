a8145b162052a21feb89d029c0ee1c82366867d4
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-06-13 12:59:52 +0100

    style: Remove comment

--- src/App.js
+++ src/App.js
@@ -24,7 +24,5 @@ export default App;
 To Do:
 1. Google Login - errorHandle. Pause. If doesn't work provide a message:
 This is a demo. Google Login not necessary. Redirecting to the app.
-3. Profile - if user has not provided any stats, make the update your stats button
-bounce every 3 seconds
 4. Place an x button on popup in profile
 */
