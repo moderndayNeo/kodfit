d32ee2fdd2fe21e0929f62ad1c0e1a423f2a02f7
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-06-13 12:57:34 +0100

    refactor: Animate PopupButton if user info needed

--- src/PageView/Profile/PopupButton/PopupButton.js
+++ src/PageView/Profile/PopupButton/PopupButton.js
@@ -1,26 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import './PopupButton.css';
+import React from 'react'
+import './PopupButton.css'
 
 export default function PopupButton({ onClick, infoProvided }) {
-    const [animated, setAnimated] = useState(true)
-
-    // useEffect(() => {
-    //     setInterval(() => {
-    //         if (infoProvided === false) {
-    //             setAnimated(true)
-    //         }
-    //     }, 3000);
-    //     setTimeout(() => {
-    //         setAnimated(false)
-    //     }, 1000);
-    // }, [infoProvided])
-
     return (
         <button
-            className={`PopupButton button primary rounded-capsule update-stats ${animated && "call-user-attention"}`}
+            className={`PopupButton button primary rounded-capsule update-stats ${
+                !infoProvided && 'call-user-attention'
+            }`}
             onClick={onClick}
         >
             Update your stats
         </button>
-    );
+    )
 }
