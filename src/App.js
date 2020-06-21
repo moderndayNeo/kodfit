e5d7cf33465ede17d8749c4562b7a937f3d6392d
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-06-21 14:45:34 +0100

    style: Remove comments

--- src/App.js
+++ src/App.js
@@ -19,36 +19,3 @@ function App() {
     );
 }
 export default App;
-
-/*
-To Do:
-WorkoutCurrent should be a re-usable component.
-Simply provide the list of 7 exercises required. It will start the timer and run through the list, regardless of where the data came from.
-
-Click Start => Trigger workoutCurrent
-Doesn't matter if coming from Calendar, workout or exercises
-
-
-Start Button - takes href as props
-
-Calendar => href to workout-current
-Workouts/Exercises => href to workout-current/listId
-
-/dashboard/workouts/home/CoreAbs
-/dashboard/workouts/exercise-list/Chest
-
-On workouts/home or workouts/exercise list, for each page, create
-a listId variable and assign it the listID from the url parameters
-
-Each ExerciseList component has a Start Button that redirects to
-/current-workout/listId
-
-
-On current-workout/listId, the current workout component takes the listID
-from the url. It then checks the exercise data for exercises with the
-workout or category that match the listId.
-It then begins the timer and displays those exercises
-
-
-workoutCurrent/listId
-*/
