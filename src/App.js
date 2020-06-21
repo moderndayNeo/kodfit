6988ba6572f93c231077d5bbd77b6d75ab99111b
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-06-21 11:38:18 +0100

    refactor: Change className (CalendarWorkoutSection)

--- src/App.js
+++ src/App.js
@@ -34,5 +34,21 @@ Start Button - takes href as props
 Calendar => href to workout-current
 Workouts/Exercises => href to workout-current/listId
 
+/dashboard/workouts/home/CoreAbs
+/dashboard/workouts/exercise-list/Chest
+
+On workouts/home or workouts/exercise list, for each page, create
+a listId variable and assign it the listID from the url parameters
+
+Each ExerciseList component has a Start Button that redirects to
+/current-workout/listId
+
+
+On current-workout/listId, the current workout component takes the listID
+from the url. It then checks the exercise data for exercises with the
+workout or category that match the listId.
+It then begins the timer and displays those exercises
+
+
 
 */
