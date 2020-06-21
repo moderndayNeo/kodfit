4ae8160fc7bb9db6bd6c7746e5d1384bf260630c
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-06-21 14:40:38 +0100

    feat: Dynamically display WorkoutCurrent based on chosen workout

--- src/PageView/WorkoutsMain/WorkoutCurrent/WorkoutCurrent.js
+++ src/PageView/WorkoutsMain/WorkoutCurrent/WorkoutCurrent.js
@@ -1,71 +1,71 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react'
 import { ExerciseContext } from '../../../context/ExerciseContext'
-import ExercisesData from '../../../Data/ExercisesData';
-import SmallTimer from '../../Timer/SmallTimer/SmallTimer';
-import './WorkoutCurrent.css';
+import ExercisesData from '../../../Data/ExercisesData'
+import SmallTimer from '../../Timer/SmallTimer/SmallTimer'
+import './WorkoutCurrent.css'
 import { useLocation } from 'react-router-dom'
 
 export default function WorkoutCurrent() {
     const location = useLocation()
-    const slug = location.pathname.split('/').pop() // random, CoreAbs, shoulders
+    const slug = location.pathname.split('/').pop()
+    const filtered = ExercisesData()
+        .filter(
+            (exercise) =>
+                exercise.category === slug || exercise.workout === slug
+        )
+        .map((exercise) => exercise.exerciseName)
+    const randomExercises = useContext(ExerciseContext)
+    let [position, setPosition] = useState(0)
+    let time = 30000
 
-    const randomExercises = useContext(ExerciseContext); // An array of seven random numbers
-    // const randomValues = useContext(ExerciseContext)
-    // const randomExercises = slug === 'random' ? randomValues : [0,1,2,3,4,5,6]
-    // console.log(randomExercises)
-
-    let [position, setPosition] = useState(0);
-    let time = 30000;
-
-  useEffect(() => {
-        if(position <= randomExercises.calendarExercisesList.length) { 
+    useEffect(() => {
+        if (
+            position <= randomExercises.calendarExercisesList.length ||
+            position <= filtered.length
+        ) {
             const interval = setInterval(() => {
-                setPosition(position + 1);
-            }, time);
-            return () => clearInterval(interval);
+                setPosition(position + 1)
+            }, time)
+            return () => clearInterval(interval)
         }
-   }, [position, randomExercises.calendarExercisesList.length, time]);
+    }, [
+        position,
+        randomExercises.calendarExercisesList.length,
+        filtered.length,
+        time,
+    ])
 
-    let itemIndex = randomExercises.calendarExercisesList[position];
+    let itemIndex =
+        slug === 'random'
+            ? randomExercises.calendarExercisesList[position]
+            : position
 
     return (
         <div className="WorkoutCurrent container">
-            <h2 className="separator-center">
-                Workout Generator!
-            </h2>
+            <h2 className="separator-center">Workout Generator!</h2>
             <main>
                 <div className="containerOne-content workoutAction">
-
-                   
-                    
-                    {
-                        itemIndex && 
-                        <>  
-                            <h2>#{itemIndex}</h2>
-                            <h2 className='workout-name'>
-                                {ExercisesData()[itemIndex].exerciseName}
-                                {/* {
-                                    slug === 'random' ?
-                                    ExercisesData()[itemIndex].exerciseName :
-                                    ExercisesData().filter(exercise => exercise.category === listID || exercise.workout === listID)[itemIndex].exerciseName
-                                } */}
-
+                    {slug === 'random' ? (
+                        itemIndex && (
+                            <>
+                                <h2>#{itemIndex}</h2>
+                                <h2 className="workout-name">
+                                    {ExercisesData()[itemIndex].exerciseName}
+                                </h2>
+                                <SmallTimer timer={time / 1000} />
+                            </>
+                        )
+                    ) : (
+                        <>
+                            <h2>#{itemIndex + 1}</h2>
+                            <h2 className="workout-name">
+                                {filtered[itemIndex]}
                             </h2>
-                            <SmallTimer timer={time / 1000}/>
+                            <SmallTimer timer={time / 1000} />
                         </>
-                    }
-
-
-
-
-
-                </div >
-            </main>  
+                    )}
+                </div>
+            </main>
         </div>
     )
 }
-
-
-/*
-Context: calendarExercisesList: getRandomNumbersArray(7)
-*/
